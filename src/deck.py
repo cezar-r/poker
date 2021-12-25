9c2c67db984cb1dfff7204cad0325bd8e8f05609
cezar-r <59450965+cezar-r@users.noreply.github.com>
2021-12-24 17:09:36 -0700

    Update deck.py

--- src/deck.py
+++ src/deck.py
@@ -1,13 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+This file contains the Deck object
+'''
+
 from cards import SUITS, VALUES, Card
 import random
 
 
 class Deck:
-
+	"""
+	A class that represents a deck
+	
+	Attributes
+	----------
+	cards : list
+		list of all cards
+	
+	Methods
+	-------
+	_init_cards()
+		Adds all the cards to the deck
+	shuffle()
+		Shuffles the list of cards
+	reset()
+		Calls the _init_cards() method
+	"""
 	def __init__(self):
 		self.cards = self._init_cards()
 
 	def _init_cards(self):
+		"""
+		Adds all the cards to the deck
+		"""
 		cards = []
 		for suit in SUITS:
 			for value in VALUES:
@@ -15,9 +40,15 @@ class Deck:
 		return cards
 
 	def shuffle(self):
+		"""
+		Shuffles the list of cards
+		"""
 		random.shuffle(self.cards)
 
 
 	def reset(self):
+		"""
+		Calls the _init_cards() method
+		"""
 		self.cards = self._init_cards()
 
