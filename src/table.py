3f39223d8c44ea66de4c075214d942124c066335
cezar-r <59450965+cezar-r@users.noreply.github.com>
2021-12-24 18:59:50 -0700

    Update table.py

--- src/table.py
+++ src/table.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""File containing the table object"""
 import random
 import os
 import time
@@ -8,6 +11,38 @@ from winner_calculator import WinnerCalculator
 
 
 class Table:
+	"""
+	A class representing the table
+	
+	Attributes
+	----------
+	players : list
+		List of all players in game
+	players_in_hand : list
+		List of players in current game
+	_cards : list
+		List of cards representing the deck
+	pot_value : int
+		Amount of money in pot
+	big_blind_position : int
+		Position of big blind
+	community_cards : list
+		List of cards on board
+	big_blind_amount : int
+		Amount the big blind is
+	little_blind_amount : int
+		Amount the little blind is
+	call_amount : int
+		Amount players need to call
+	turn : str
+		Current turn of the game
+	prev_player : Player()
+		previous player in the turn
+	prev_player_decision : list
+		Represents the previous players decision ([Decision, Amount raised])
+	games : int
+		Number of games played
+	"""
 
 	def __init__(self, players, deck, buyin):
 		self.players = players
@@ -28,6 +63,7 @@ class Table:
 		self.games = 0
 
 	def play(self):
+		"""Method that plays each turn"""
 		self.community_cards = []
 		self.move_blinds()
 		self.players_in_hand = self.players.copy()
@@ -113,6 +149,7 @@ class Table:
 		self.games += 1
 
 	def _reset_table(self, done = False):
+		"""Method that resets the table back to the initial state"""
 		self.display(done = done)
 		if done:
 			self.calculate_winner() 
@@ -125,6 +162,7 @@ class Table:
 		self.games += 1
 
 	def _reset_players(self):
+		"""Method that resets all players to their initial state"""
 		for player in self.players:
 			if player.amount < self.little_blind_amount: # player busted
 				del self.players[self.players.index(player)]
@@ -132,6 +170,7 @@ class Table:
 				player.reset() 
 
 	def play_again(self):
+		"""Method that checks to see if all players want to play again"""
 		if self._humanCount() == 0:
 			# play_again = input(f"Play again? [Y][N]\n")
 			play_again = 'y'
@@ -145,18 +184,20 @@ class Table:
 			play_again = player.play_again()
 			if play_again:
 				if play_again == 'a':
-					self._play_all()
 					return 
 				new_players.append(player)
 		self.players = new_players
 
-	def _play_all(self):
-		new_players = []
-		for player in self.players:
-			new_players.append(player)
-		self.players = new_players
 
 	def _bet(self, i: int):
+		"""
+		Method that plays a round of betting
+		
+		Parameters
+		----------
+		i : int
+			Represents the first player to go in the turn
+		"""
 		betting_list = self._resort_players(i)
 		to_call = betting_list.copy()
 		i = 0
@@ -210,6 +251,21 @@ class Table:
 
 
 	def _get_next_index(self, name, array): 
+		"""
+		Method that gets the index of the next player 
+		
+		Parameters
+		----------
+		name : str
+			Name of current player
+		array : list
+			List of players to bet
+		
+		Returns
+		-------
+		int 
+			i + 1 if player is not at the end of the table, otherwise 0
+		"""
 		for i, player in enumerate(array):
 			if player.name == name:
 				if i == len(array) - 1:
@@ -218,10 +274,21 @@ class Table:
 					return i + 1
 
 	def _reset_put_in(self):
+		"""Method that resets the amount put in by each player"""
 		for player in self.players_in_hand:
 			player.put_in = 0
 
 	def _update_meta_player(self, name, amount):
+		"""
+		Method pdates the amount in each player
+		
+		Parameters
+		----------
+		name : str
+			Name of current player
+		amount : int
+			Amount of money to add to players balance
+		"""
 		for player in self.players:
 			if player.name == name:
 				player.amount = amount
@@ -229,23 +296,34 @@ class Table:
 				del self.players[self.players.index(player)]
 
 	def _remove_player(self, name):
+		"""Method that removes a player from the table"""
 		for i in range(len(self.players_in_hand) - 1):
 			if self.players_in_hand[i].name == name:
 				del self.players_in_hand[i]
 
 	def calculate_winner(self, verbose = True):
+		"""Method that calculates who the winner is and the winnings they recieve"""
 		calculator = WinnerCalculator(self.players_in_hand, self.community_cards, verbose)
 		for winner in calculator.winners:
 			winner.amount += self.pot_value / len(calculator.winners)
 			self._update_meta_player(winner.name, winner.amount)
 
 	def _resort_players(self, i, players = None):
+		"""
+		Method that resorts the betting order of the players
+		
+		i : int
+			index of current player
+		players : list
+			List of players in hand
+		"""
 		if not players:
 			return self.players_in_hand[i:] + self.players_in_hand[:i]
 		else:
 			return players[i:] + players[:i]
 
 	def move_blinds(self):
+		"""Method that moves the blind positions"""
 		self._reset_blinds()
 
 		if self.big_blind_position >= len(self.players) - 1:
@@ -261,6 +339,7 @@ class Table:
 			self.players[self.big_blind_position + 1].is_little_blind = True
 
 	def _reset_blinds(self):
+		"""Method that resets blind attributes for players"""
 		for player in self.players:
 			if player.is_big_blind:
 				player.is_big_blind = False
@@ -268,9 +347,23 @@ class Table:
 				player.is_little_blind = False
 
 	def _init_deck(self, deck):
+		"""Method that initalizes the deck"""
 		return deck.cards
 
 	def _humanCount(self, arr = []):
+		"""
+		Method that counts how many real players are in the hand
+		
+		Parameters
+		----------
+		arr : list
+			List of players in hand
+		
+		Returns
+		-------
+		int
+			Number of real players in hand
+		"""
 		if arr == []:
 			arr = self.players
 		count = 0
@@ -279,7 +372,21 @@ class Table:
 				count += 1
 		return count
 
-	def display(self, cur_player = Player("doe"), odds = False, arr = [], prev_player = [Player("doe"), ["", ""]], done = False):
+	def display(self, cur_player = Player("doe"), odds = False,  prev_player = [Player("doe"), ["", ""]], done = False):
+		"""
+		Method that displays the game
+		
+		Parameters
+		----------
+		cur_player : Player()
+			Current player in turn
+		odds : bool
+			Whether or not to display odds
+		prev_player : List
+			List that contains previous player and previous decision
+		done : bool
+			Whether or not the hand got to showdown
+		"""
 		if not cur_player.is_bot and cur_player.name != "":
 			if self._humanCount() > 1:
 				self.look_away(cur_player)
@@ -317,6 +424,7 @@ class Table:
 			print("Winner:", self.players_in_hand[0].name, "\n")
 
 	def look_away(self, cur_player):
+		"""Method that tells a real player to look away from the screen when it is the other players turn"""
 		os.system('cls' if os.name == 'nt' else 'clear')
 		print(f"Everyone but {cur_player.name} look away")
 		input("Press any key to continue")
